78143ac700d1554e989042d71382b55b3ee7e2e5
CarlosCubNed <97036823+carloscfgos1980@users.noreply.github.com>
2022-08-06 06:17:15 +0200

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,1 +1,28 @@
+1. Function to display log in or log out. This is in Reac. Check the following site for explanation.
 
+https://scrimba.com/scrim/cewRpUQ?pl=p7P5Hd
+
+this.state = {
+            isLoggedIn: false
+        }
+
+     let wordDisplay
+        if (this.state.isLoggedIn) {
+            wordDisplay = "in"
+        } else {
+            wordDisplay = "out"
+        }
+        return (
+            <div>
+                <h1>You are currently logged {wordDisplay}</h1>
+            </div>
+        )
+    }
+}
+
+
+
+
+
+
+2. 
