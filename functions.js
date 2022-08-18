23c0aeeeee806e88e5fdf7edb507ecc2c03a1ffe
CarlosCubNed <97036823+carloscfgos1980@users.noreply.github.com>
2022-08-18 04:50:37 +0200

    Update functions.js

--- functions.js
+++ functions.js
@@ -25,4 +25,24 @@ this.state = {
 
 
 
-2. 
+2. Time of the day in React
+
+https://scrimba.com/scrim/c6G36SV?pl=p7P5Hd
+
+function App() {
+  const date = new Date()
+  const hours = date.getHours()
+  let timeOfDay
+  
+  if (hours < 12) {
+    timeOfDay = "morning"
+  } else if (hours >= 12 && hours < 17) {
+    timeOfDay = "afternoon"
+  } else {
+    timeOfDay = "night"
+  }
+  
+  return (
+    <h1>Good {timeOfDay}!</h1>
+  )
+}
